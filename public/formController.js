8adc86ced672d4d718c67fed651110f42c100d15
AaronKolenda <kolenda.a@gmail.com>
2015-03-18 21:02:07 -0400

    hard mode

--- public/formController.js
+++ public/formController.js
@@ -16,6 +16,39 @@ $scope.email = "Email";
 $scope.website = "Website";
 $scope.message = "Message";
 
+
+
+$scope.$watchGroup(['name', 'email', 'website', 'message'], function(newValue, oldValue) {
+	$scope.nameInvalid = false;
+	$scope.emailInvalid = false;
+	$scope.websiteInvalid = false;
+	$scope.messageInvalid = false;
+
+	if ($scope.name === undefined || $scope.name.trim() === "") {
+		$scope.nameInvalid = true;
+	}
+	if ($scope.email !== undefined || $scope.email.trim() !== "") {
+		if ($scope.email.indexOf("@") === -1) {
+			$scope.emailInvalid = true;
+		}
+	}
+	if ($scope.email === undefined || $scope.email.trim() === "") {
+		$scope.emailInvalid = true;
+	}
+	if ($scope.website !== undefined || $scope.website.trim() !== "") {
+		if ($scope.website.indexOf("http://") === -1) {
+			$scope.websiteInvalid = true;
+		}
+	}
+	if ($scope.website === undefined || $scope.website.trim() === "") {
+		$scope.websiteInvalid = true;
+	}
+	if ($scope.message === undefined || $scope.message.trim() === "") {
+		$scope.messageInvalid = true;
+	}
+});
+
+
 $scope.submitForm = function() {
 
 	$scope.nameError = false;
